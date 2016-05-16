717b080fd110fc8e1f7b3c892e96eef8cd08b225
Jesper <Jesper@192.168.2.3>
2016-05-16 21:10:26 +0200

    lmao

--- src/gui/Editor.java
+++ src/gui/Editor.java
@@ -3,8 +3,16 @@ package gui;
 import java.awt.Color;
 import javax.swing.JTextField;
 
+import model.Sheet;
+
 public class Editor extends JTextField {
-    public Editor() {
+    private Sheet sheet;
+	private CurrentSlot currentSlot;
+
+	public Editor(CurrentSlot currentSlot, Sheet sheet) {
         setBackground(Color.WHITE);
+        this.currentSlot = currentSlot;
+        this.sheet = sheet;
+        
     }
 }
