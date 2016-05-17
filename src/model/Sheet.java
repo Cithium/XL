ad3132056272bc5a7ebd9d89071e738cfbccb8b8
HamzaAbdulilah <hamza.abdulilah@gmail.com>
2016-05-17 22:06:59 +0200

    loadMap errorMessages

--- src/model/Sheet.java
+++ src/model/Sheet.java
@@ -105,7 +105,7 @@ public class Sheet extends Observable implements Environment {
 		while(itr.hasNext()&&!errorInEntry){
 			Entry<String,Slot> entry = itr.next();
 			if(circularCheck(entry.getKey(),entry.getValue())){
-				errorMessage = "Cannot load these files";
+				errorMessage = errorMessage +"Cannot load these files";
 				this.sheetMap = temp;
 				errorInEntry=true;
 			}
@@ -114,7 +114,7 @@ public class Sheet extends Observable implements Environment {
 		sheetMap = map;
 		setChanged();
 		notifyObservers();
-		
+		errorMessage="";
 
 		
 	}
