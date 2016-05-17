358726f16497e0b5d51de593e55b9d2dea98f7af
HamzaAbdulilah <hamza.abdulilah@gmail.com>
2016-05-17 15:30:46 +0200

    Sheet methods

--- src/model/Sheet.java
+++ src/model/Sheet.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Observable;
 
 import expr.Environment;
+import gui.SlotLabel;
 import util.XLException;
 
 public class Sheet extends Observable implements Environment {
@@ -17,22 +18,36 @@ public class Sheet extends Observable implements Environment {
 	public void editSlot(String key, String input) {
 		//Slot value = SlotFactory.create(text) // Hitta rätt typ av slot...
 		Slot value = SlotTypeChecker.check(input);
-		if (circularCheck(key, value)) {
-			System.out.println("CHECK FEL LOL");
-		} else {
-			sheetMap.put(key, value);
-		}
+		
+		sheetMap.put(key, value);
 	//	sheetMap.put("A1", new TextSlot("123"));
+	}
+	
+	public boolean insert(SlotLabel sl, Slot s){
+		return true;
+	}
+	
+	public boolean clearSlot(SlotLabel sl, Slot s){
+		return true;
+	}
+	
+	public boolean clearAll(){
+		return false;
+		
+	}
+	
+	public boolean loadSlots(String s){
+		return true;
+	}
+	public boolean saveSlots(String s){
+		return true;
 	}
 	
 	public double value(String text) {
 		System.out.print("TEST  :: ");
 		System.out.println(text);
 		Slot testSlot = sheetMap.get(text);
-	/*	if (sheetMap.get(text) == null) {
-			throw new XLException(text + " does not exist in the sheet.");
-		}
-	*/	return sheetMap.get(text).value(this);
+		return sheetMap.get(text).value(this);
 	}
 	
 	public String print(String key) {
@@ -45,27 +60,6 @@ public class Sheet extends Observable implements Environment {
 		} catch (XLException e) {
 			return "ERROR";
 		}
-	}
-	
-	public boolean circularCheck(String key, Slot value) {
-        Slot currentSlot = sheetMap.get(key);
-
-        sheetMap.put(key, new TestSlot());
-
-        try {
-            value.value(this);
-        } catch (XLException e) {
-        	System.out.println("Bad input 1");
-            //currentError = "Bad input, ";
-            return true;
-        } catch (NullPointerException e) {
-        	System.out.println("Bad input 2");
-            //currentError = "Bad input, ";
-            return true;
-        }
-
-        sheetMap.put(key, currentSlot);
-        return false;
 	}
 	
 	
